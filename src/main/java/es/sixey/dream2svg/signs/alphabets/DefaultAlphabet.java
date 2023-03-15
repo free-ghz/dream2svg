ec8bf1293db6b1f8e4a0c12d3d035dda7bfc4502
free-ghz <elmusho@gmail.com>
2023-03-15 20:26:38 +0100

    Add letters b, d, h, n, p, q, r, s, w to default alphabet

--- src/main/java/es/sixey/dream2svg/signs/alphabets/DefaultAlphabet.java
+++ src/main/java/es/sixey/dream2svg/signs/alphabets/DefaultAlphabet.java
@@ -57,6 +57,12 @@ public class DefaultAlphabet implements Alphabet {
                     new Path().addPoint(0,1).addPoint(0.5,0).addPoint(1, 1),
                     new Path().addPoint(0.25,0.5).addPoint(0.75, 0.5)
             ));
+            case 'b' -> new Letter(List.of(
+                    new Path().addPoint(0, 0).addPoint(0, 1).addPoint(0.7,1).addPoint(1, 0.7).addPoint(0, 0.5)
+            ));
+            case 'd' -> new Letter(List.of(
+                    new Path().addPoint(1, 0).addPoint(1, 1).addPoint(0.3,1).addPoint(0, 0.7).addPoint(1, 0.5)
+            ));
             case 'e' -> new Letter(List.of(
                     new Path().addPoint(1, 0).addPoint(0.5, 0).addPoint(0, 0.5).addPoint(0.5, 1).addPoint(1, 1),
                     new Path().addPoint(0, 0.5).addPoint(1, 0.5)
@@ -64,12 +70,37 @@ public class DefaultAlphabet implements Alphabet {
             case 'f' -> new Letter(List.of(
                     new Path().addPoint(0.1, 1).addPoint(0.1, 0).addPoint(0.5, 0).addPoint(0, 0.55).addPoint(1, 0.45)
             ));
+            case 'h' -> new Letter(List.of(
+                    new Path().addPoint(0, 0).addPoint(0, 1),
+                    new Path().addPoint(0, 0.6).addPoint(0.7, 0.4).addPoint(0.9, 0.6).addPoint(1, 1)
+            ));
             case 'i' -> new Letter(List.of(
                     new Path().addPoint(0.5, 0.2).addPoint(0.4, 0.1).addPoint(0.5, 0).addPoint(0.6, 0.1).addPoint(0.5, 0.2).addPoint(0.5, 1)
+            ));
+            case 'n' -> new Letter(List.of(
+                    new Path().addPoint(0, 0.2).addPoint(0.2, 0).addPoint(0.2, 1),
+                    new Path().addPoint(0.2, 0.2).addPoint(0.4, 0).addPoint(0.7, 0).addPoint(1, 0.15).addPoint(1, 1)
+            ));
+            case 'p' -> new Letter(List.of(
+                    new Path().addPoint(0, 1).addPoint(0, 0).addPoint(0.7,0).addPoint(1, 0.3).addPoint(0, 0.5)
+            ));
+            case 'q' -> new Letter(List.of(
+                    new Path().addPoint(1, 1).addPoint(1, 0).addPoint(0.3,0).addPoint(0, 0.3).addPoint(1, 0.5),
+                    new Path().addPoint(1, 0.7).addPoint(0.8, 0.7)
+            ));
+            case 'r' -> new Letter(List.of(
+                    new Path().addPoint(0, 1).addPoint(0, 0),
+                    new Path().addPoint(0, 0.1).addPoint(0.7, 0).addPoint(0.9, 0.15).addPoint(1, 0.4)
+            ));
+            case 's' -> new Letter(List.of(
+                    new Path().addPoint(0, 0.8).addPoint(0.2, 1).addPoint(0.8, 1).addPoint(1, 0.8).addPoint(0, 0.2).addPoint(0.2, 0).addPoint(0.8, 0).addPoint(1, 0.2).addPoint(1, 0)
             ));
             case 't' -> new Letter(List.of(
                     new Path().addPoint(0, 0).addPoint(1, 0),
                     new Path().addPoint(0.5, 0).addPoint(0.5, 1)
+            ));
+            case 'w' -> new Letter(List.of(
+                    new Path().addPoint(0, 0).addPoint(0.25, 1).addPoint(0.5, 0.5).addPoint(0.75, 1).addPoint(1, 0)
             ));
             case 'y' -> new Letter(List.of(
                     new Path().addPoint(0, 0).addPoint(0.5, 0.5).addPoint(1, 0),
